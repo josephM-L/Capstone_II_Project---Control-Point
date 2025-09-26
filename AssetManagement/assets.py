3515e8010a2bb5f50433846f36926e1ba08d1867
josephM-L <josephmcnultylamparter@gmail.com>
2025-09-26 12:04:47 -0400

    Implemented full Add Asset form with references

--- AssetManagement/assets.py
+++ AssetManagement/assets.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask import Blueprint, render_template, redirect, request, flash
-from models import db, Asset
+from models import db, Asset, AssetType, AssetStatus, Location, Vendor, Employee
 
 assets_bp = Blueprint("assets", __name__)
 
@@ -13,6 +13,14 @@ def assets():
         purchase_date = request.form.get("purchase_date")
         purchase_cost = request.form.get("purchase_cost")
         serial_number = request.form.get("serial_number")
+        warranty_expiry = request.form.get("warranty_expiry")
+
+        # Foreign key references
+        asset_type_id = request.form.get("asset_type_id")
+        status_id = request.form.get("status_id")
+        location_id = request.form.get("location_id")
+        vendor_id = request.form.get("vendor_id")
+        assigned_to = request.form.get("assigned_to")
 
         if not asset_tag or not name:
             flash("Asset tag and name are required!", "danger")
@@ -20,6 +28,7 @@ def assets():
 
         try:
             purchase_date_parsed = datetime.strptime(purchase_date, "%Y-%m-%d").date() if purchase_date else None
+            warranty_expiry_parsed = datetime.strptime(warranty_expiry, "%Y-%m-%d").date() if warranty_expiry else None
             purchase_cost_val = float(purchase_cost) if purchase_cost else None
 
             new_asset = Asset(
@@ -28,7 +37,13 @@ def assets():
                 description=description,
                 purchase_date=purchase_date_parsed,
                 purchase_cost=purchase_cost_val,
-                serial_number=serial_number
+                serial_number=serial_number,
+                warranty_expiry=warranty_expiry_parsed,
+                asset_type_id=int(asset_type_id) if asset_type_id else None,
+                status_id=int(status_id) if status_id else None,
+                location_id=int(location_id) if location_id else None,
+                vendor_id=int(vendor_id) if vendor_id else None,
+                assigned_to=int(assigned_to) if assigned_to else None,
             )
 
             db.session.add(new_asset)
@@ -42,7 +57,24 @@ def assets():
 
     # Query all assets
     asset_query = Asset.query.order_by(Asset.asset_id).all()
-    return render_template("assets.html", assets=asset_query)
+
+    # Query related tables for dropdowns
+    asset_types = AssetType.query.order_by(AssetType.name).all()
+    statuses = AssetStatus.query.order_by(AssetStatus.status_name).all()
+    locations = Location.query.order_by(Location.name).all()
+    vendors = Vendor.query.order_by(Vendor.name).all()
+    employees = Employee.query.order_by(Employee.last_name, Employee.first_name).all()
+
+    return render_template(
+        "assets.html",
+        assets=asset_query,
+        asset_types=asset_types,
+        statuses=statuses,
+        locations=locations,
+        vendors=vendors,
+        employees=employees
+    )
+
 
 @assets_bp.route("/assets/delete/<int:asset_id>", methods=["GET", "POST"])
 def delete_asset(asset_id):
